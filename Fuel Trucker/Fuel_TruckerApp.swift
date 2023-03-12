a60abe209390bb09d22cfff516c07ac1f26de68c
mederland <mederland@gmail.com>
2023-03-12 17:06:39 -0500

    Initial Commit

---
+++ Fuel Trucker/Fuel_TruckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fuel_TruckerApp.swift
+//  Fuel Trucker
+//
+//  Created by Meder iZimov on 3/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Fuel_TruckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
